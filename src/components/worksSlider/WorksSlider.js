1c39312f4ee077c304d25f94e786982e2d17fe4a
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-03-25 13:25:37 +0600

    scroll to component

--- src/components/worksSlider/WorksSlider.js
+++ src/components/worksSlider/WorksSlider.js
@@ -41,7 +41,14 @@ const WorksSlider = () => {
             <H2 data="Наши работы" />
             <div className="worksSlider_container">
             
-            <Carousel responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={3000} transitionDuration={500}>
+            <Carousel 
+            responsive={responsive} 
+            infinite={true} 
+            autoPlay={true} 
+            autoPlaySpeed={3000} 
+            transitionDuration={500}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            >
                 {list.map(({id, name, image}) => (
                     <NavLink to={`/categories/${id}`}>
                         <div className='worksSlider_item'>

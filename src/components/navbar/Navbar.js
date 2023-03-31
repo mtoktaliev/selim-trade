346d2693738a8d8e4dcf661c214559a2255256b4
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-04-01 03:21:06 +0600

    check styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-scroll';
+import { CiPhone } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
 
 import { ROUTES } from '../../utils/Routes'
 import "./Navbar.css";
@@ -31,35 +33,35 @@ const Navbar = () => {
             })
         }
     }
-        return (
-            <nav className="nav">
-                <div className='nav_container'>
-                   
-                    <NavLink to={ROUTES.HOME} className="nav_link hide_active"><img src={logo} alt="logo"></img></NavLink>
-                    
-                    <ul className='nav_list' style={hideOrSwow} onClick={handleMenu}>
-                        <NavLink to={ROUTES.HOME} className='nav_link'>Главная</NavLink>
-                        <NavLink to={ROUTES.GATES} className='nav_link'>Услуги</NavLink>
-                        <NavLink to={ROUTES.NEWSPAGE} className='nav_link'>Новости</NavLink>
-                        <NavLink to={ROUTES.WORKS} className='nav_link'>Наши работы</NavLink>
-                        <NavLink to={ROUTES.ADMINPANEL} className='nav_link'>Админ панель</NavLink>
-                        <div className="contacts">
-                            <Link className='nav_link' to="map" spy={true} smooth={true} offset={50} duration={500} onClick={handleMenu}>г. Бишкек</Link>
-                            <NavLink className='nav_link' to="tel:+996552570755"><br/>+996 (552) 57 07 55</NavLink>
-                        </div>
-                    
-                    </ul>
-                    
-                    
-                    {isOpen ? <div className='nav_toggle open'onClick={handleMenu}>
-                        <div className="bar"></div>
-                    </div> : <div className='nav_toggle'onClick={handleMenu}>
-                        <div className="bar"></div>
-                    </div>}
-               
-                </div>
-            </nav>
-        );
+    return (
+        <nav className="nav">
+            <div className='nav_container'>
+                
+                <NavLink to={ROUTES.HOME} className="nav_link hide_active"><img src={logo} alt="logo"></img></NavLink>
+                
+                <ul className='nav_list' style={hideOrSwow} onClick={handleMenu}>
+                    <NavLink to={ROUTES.HOME} className='nav_link'>Главная</NavLink>
+                    <NavLink to={ROUTES.GATES} className='nav_link'>Услуги</NavLink>
+                    <NavLink to={ROUTES.NEWSPAGE} className='nav_link'>Новости</NavLink>
+                    <NavLink to={ROUTES.WORKS} className='nav_link'>Наши работы</NavLink>
+                    <div className='nav_link_contacts'>
+                        <Link className='nav_link' style={{fontSize: '15px'}} to="map" spy={true} smooth={true} offset={50} duration={500} onClick={handleMenu}><CiLocationOn/>&thinsp;г. Бишкек</Link>
+                        <NavLink className='nav_link' style={{fontSize: '15px'}} to="tel:+996552570755"><CiPhone/>&thinsp;+996 (552) 57 07 55</NavLink>
+                    </div>
+                    <NavLink to={ROUTES.ADMINPANEL}><button className='sign_btn_toggle'>Войти</button></NavLink>
+                </ul>
+                    <NavLink to={ROUTES.ADMINPANEL}><button className='sign_btn'>Войти</button></NavLink>
+                
+                
+                {isOpen ? <div className='nav_toggle open'onClick={handleMenu}>
+                    <div className="bar"></div>
+                </div> : <div className='nav_toggle'onClick={handleMenu}>
+                    <div className="bar"></div>
+                </div>}
+            
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
